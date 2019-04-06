5131c6e6295f794ac5758a46751d074811150423
Stefan Dombrowski <sdo451@gmail.com>
2019-04-06 17:14:57 +0200

    Entities

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import StoreProvider from './StoreProvider';
 import Player from './components/Player';
 import Map from './components/Map';
+import Entities from "./components/Entities.js";
 
 function App() {
   return (
     <StoreProvider>
       <div style={{ display: 'relative' }}>
         <Map />
+        <Entities />
         <Player />
       </div>
     </StoreProvider>
