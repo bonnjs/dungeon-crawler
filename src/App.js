6e29ab96796ef7579e09849783358245f12451dc
Simon Altrogge <simonaltrogge@gmail.com>
2019-04-06 12:42:27 +0200

    Add useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
-import React, { Component } from 'react';
+import React, { useState, useReducer } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const MOVE_UP = 'MOVE_UP';
+const MOVE_DOWN = 'MOVE_DOWN';
+const MOVE_LEFT = 'MOVE_LEFT';
+const MOVE_RIGHT = 'MOVE_RIGHT';
+
+function App() {
+  const [state, dispatch] = useReducer(
+    (state, action) => {
+      switch (action.type) {
+        case MOVE_UP: {
+          return state;
+        }
+        case MOVE_DOWN: {
+          return state;
+        }
+        case MOVE_LEFT: {
+          return state;
+        }
+        case MOVE_RIGHT: {
+          return state;
+        }
+        default: {
+          return state;
+        }
+      }
+    },
+    {
+      tiles: [
+        0, 1, 1, 1, 1, 0, 0, 0, 0,
+        0, 1, 0, 0, 1, 0, 0, 0, 0,
+        0, 1, 0, 0, 1, 0, 0, 0, 0,
+        0, 1, 1, 1, 1, 0, 0, 0, 0,
+      ],
+      width: 9,
+      entities: [{ type: '', position: [0, 0] }],
+      player: { position: [0, 0], health: 100 },
+    }
+  );
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
